2e5043ad2f091f8cc6eb96f945a89fa1a13f9787
Hey, I am Aditya !!! <111901005@smail.iitpkd.ac.in>
2022-01-22 22:32:42 +0530

    Time: 896 ms (6.04%), Space: 6.8 MB (57.72%) - LeetHub

---
+++ 1510-stone-game-iv/1510-stone-game-iv.cpp
@@ -0,0 +1,58 @@
+
+
+class Solution {
+public:
+    
+int dp[100001];
+
+vector<int>v;
+
+void pre(){
+
+    dp[1] = 1;
+    
+    for(int i=1;i*i<100001;i++){
+        v.push_back(i*i);
+
+    }
+
+
+    for(int i=2;i<100001;i++){
+
+        dp[i] = -1;
+
+        int fl = 1;
+        for(auto x:v){
+
+            if(x>i){
+                break;
+            }
+
+            if(dp[i-x]==0){
+
+                dp[i] = 1;
+                fl  =0;
+                break;
+            }
+        }
+
+        if(fl){
+            dp[i] = 0;
+        }
+
+    }
+        return;
+    }
+    
+    bool winnerSquareGame(int n) {
+        
+        pre();
+        
+        if(dp[n]==1){
+            return 1;
+        }
+        else{
+            return 0;
+        }
+    }
+};
