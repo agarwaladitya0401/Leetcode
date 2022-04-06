daaf7fca3b2248b10ddc88fc3a10388d96758502
Hey, I am Aditya !!! <111901005@smail.iitpkd.ac.in>
2022-04-07 01:51:36 +0530

    Time: 97 ms (48.69%), Space: 48.4 MB (74.45%) - LeetHub

--- 55-jump-game/55-jump-game.cpp
+++ 55-jump-game/55-jump-game.cpp
@@ -1,26 +1,26 @@
 class Solution {
 public:
-    int dp[10001];
-    bool fun(vector<int>&a, int i, int n){
-        if(i==n-1){
-            return 1;
-        }
+//     int dp[10001];
+//     bool fun(vector<int>&a, int i, int n){
+//         if(i==n-1){
+//             return 1;
+//         }
         
-        if(dp[i]!=-1){
-            return dp[i];
-        }
+//         if(dp[i]!=-1){
+//             return dp[i];
+//         }
         
-        int go = min(a[i],n-i);
+//         int go = min(a[i],n-i);
         
-        for(int j=1;j<=go;j++){
+//         for(int j=1;j<=go;j++){
             
-            if(fun(a,i+j,n)){
-                return dp[i] = 1;
-            }
-        }
+//             if(fun(a,i+j,n)){
+//                 return dp[i] = 1;
+//             }
+//         }
         
-        return dp[i] = 0;
-    }
+//         return dp[i] = 0;
+//     }
     bool canJump(vector<int>& nums) {
         
         // memset(dp,-1,sizeof(dp));
