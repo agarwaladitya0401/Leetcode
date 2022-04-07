8fbb33aba53a5647ec628a6dcebc0a051f6fda0b
Hey, I am Aditya !!! <111901005@smail.iitpkd.ac.in>
2022-04-07 21:05:23 +0530

    Time: 42 ms (88.49%), Space: 12.1 MB (95.81%) - LeetHub

--- 85-maximal-rectangle/85-maximal-rectangle.cpp
+++ 85-maximal-rectangle/85-maximal-rectangle.cpp
@@ -1,5 +1,14 @@
 class Solution {
 public:
+    
+    // see this explanation:https://leetcode.com/problems/maximal-rectangle/discuss/29054/Share-my-DP-solution/175299
+    
+ //    we start from the first row, and move downward;
+ // * height[i] record the current number of countinous '1' in column i;
+ // * left[i] record the left most index j which satisfies that for any index k from j to  i, height[k] >= height[i];
+ // * right[i] record the right most index j which satifies that for any index k from i to  j, height[k] >= height[i];
+ // * by understanding the definition, we can easily figure out we need to update maxArea with value (height[i] * (right[i] - left[i] + 1));
+    
     int maximalRectangle(vector<vector<char>>& matrix) {
         
         int n = matrix.size();
