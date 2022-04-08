44e2434586dd8ae3f1d758c23f0bae22dd15b7f1
Hey, I am Aditya !!! <111901005@smail.iitpkd.ac.in>
2022-04-08 12:51:45 +0530

    Time: 43 ms (59.38%), Space: 19.9 MB (63.09%) - LeetHub

--- 703-kth-largest-element-in-a-stream/703-kth-largest-element-in-a-stream.cpp
+++ 703-kth-largest-element-in-a-stream/703-kth-largest-element-in-a-stream.cpp
@@ -5,42 +5,28 @@ public:
     int kk;
     
     KthLargest(int k, vector<int>& nums) {
+        
         sort(nums.begin(),nums.end());
         
         int n = nums.size();
-        
-        // for(int i=0;i<n-k;i++){
-        //     l.insert(nums[i]);
-        // }
+ 
         
         kk = k;
         
         for(int i=max(n-k,0);i<n;i++){
-            // h.insert(nums[i]);
-            // mp[nums[i]]++;
             pq.push(nums[i]);
         }
     }
     
     int add(int val) {
         
-        // if(h.size()<kk-1){
-        //     mp[val]++;  
-        //     h.insert(val);
-        //     return 0;
-        // }
-        
         if(pq.size()==kk-1){
             pq.push(val);
-            // mp[val]++;
             
             return pq.top();
         }
         
         if(val>= pq.top()){
-            
-            cout<<"b:"<<pq.top()<<endl;
-
             
             pq.pop();
             pq.push(val);
