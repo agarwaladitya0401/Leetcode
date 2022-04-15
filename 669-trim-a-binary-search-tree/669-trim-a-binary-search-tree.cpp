81d34fdae00709617f3f88985f2b92453819c787
Hey, I am Aditya !!! <111901005@smail.iitpkd.ac.in>
2022-04-15 20:15:21 +0530

    Time: 22 ms (52.54%), Space: 24.2 MB (7.08%) - LeetHub

--- 669-trim-a-binary-search-tree/669-trim-a-binary-search-tree.cpp
+++ 669-trim-a-binary-search-tree/669-trim-a-binary-search-tree.cpp
@@ -37,8 +37,102 @@ public:
     }
     
     TreeNode* trimBST(TreeNode* root, int low, int high) {
-        TreeNode* head = fun(root, low, high);;
-        
-            return head;
-    }
+
+	if(root == nullptr) 
+		return nullptr;
+
+	// Why use a queue of double pointers ?
+	//
+	// Queue stores a copy of the (single) pointer to
+	//  current node, thus when we modify (single) pointer
+	//  to point to nullptr, we modify a copy and the original 
+	//  remains unaffected.
+	//
+	// When using a double pointer to current node,
+	//  queue still stores a copy of the double pointer.
+	//  However, both the copy and the original reference same 
+	//  memory location and thereby when we can modify the value 
+	//  stored in deferenced double pointer, we get the desired effect. 
+	queue<TreeNode**> todo{{&root}};
+
+	while(!todo.empty()) {
+
+		TreeNode** node = todo.front();
+		todo.pop();
+
+		// Check if current node is greater than
+		//  lower bound.
+		//
+		//  If not, replace current node with its
+		//   right subtree.
+		while ( (*node)->right
+			 && (*node)->val < low) 
+		{
+			// Before replacing current node with its
+			//  right subtree, it might me useful
+			//  to release allocated memory for current 
+			//  and its left subtree.
+			//
+			// This might be a good point to get further
+			//  clarifications from the interviewer.
+			*node = (*node)->right;
+		}
+
+		// If node value is still less than lower bound
+		//  we must mark the node null.
+		//
+		// WHY? Because it has no right child
+		//  and all nodes (if any) in left subtree will
+		//  be smaller than current node (BST property) and 
+		//  must be trimmed.
+		if ( (*node)->val < low ) 
+		{
+			*node = nullptr; 
+			continue;
+		}
+
+		// Check if current node is smaller than
+		//  upper bound.
+		//
+		//  If not, replace current node with its
+		//   left subtree.            
+		while ( (*node)->left
+			 && (*node)->val > high) 
+		{
+			*node = (*node)->left;
+		}
+
+		// If node value is still greater than upper bound
+		//  we must mark the node null.
+		//
+		// WHY? Because it has no left child
+		//  and all nodes (if any) in right subtree will
+		//  be greater than current node (BST property) and 
+		//  must be trimmed.
+		if ( (*node)->val > high ) 
+		{
+			*node = nullptr; 
+			continue;
+		}
+
+		// If current node is within bounds, queue its children (if any)
+		if (!((*node)->val < low || (*node)->val > high) ) 
+		{
+			if( (*node)->left != nullptr ) 
+				todo.push( &(*node)->left );
+
+			if( (*node)->right != nullptr ) 
+				todo.push( &(*node)->right );
+		}
+		// Otherwise queue current node; it would be trimmed
+		// next time it is visited
+		else
+		{
+			todo.push(node);
+		}
+	}
+
+	// return root of trimmed binary search tree
+	return root;
+}
 };
