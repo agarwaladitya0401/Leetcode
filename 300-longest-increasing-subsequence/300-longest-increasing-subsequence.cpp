3287f1c3e2aade6cf0acb6d49b3484b34d04c2d5
Hey, I am Aditya !!! <111901005@smail.iitpkd.ac.in>
2022-05-10 15:45:03 +0530

    Time: 235 ms (63.92%), Space: 10.4 MB (87.89%) - LeetHub

---
+++ 300-longest-increasing-subsequence/300-longest-increasing-subsequence.cpp
@@ -0,0 +1,25 @@
+class Solution {
+public:
+    int lengthOfLIS(vector<int>& nums) {
+        
+        int ans = 1; 
+        
+        int n = nums.size();
+        
+        int dp[n];
+        
+        for(int i=0;i<n;i++){
+            dp[i] = 1;
+            
+            for(int j=0;j<i;j++){
+                if(nums[j]<nums[i]){
+                    dp[i] = max(dp[i],dp[j]+1);
+                }
+            }
+            
+            ans = max(ans,dp[i]);
+        }
+        
+        return ans;
+    }
+};
