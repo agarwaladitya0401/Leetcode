f559929c403299ce7df589c66c78a39972187baf
Hey, I am Aditya !!! <111901005@smail.iitpkd.ac.in>
2022-05-25 03:32:38 +0530

    Time: 4 ms (73.85%), Space: 7.1 MB (78.80%) - LeetHub

--- 32-longest-valid-parentheses/32-longest-valid-parentheses.cpp
+++ 32-longest-valid-parentheses/32-longest-valid-parentheses.cpp
@@ -1,59 +1,35 @@
 class Solution {
 public:
-    int longestValidParentheses(string s) {
-        int o = 0;
-        int st = 0, l=0;
-        int ans = 0;
-        int n = s.size();
+    int longestValidParentheses(string s) 
+    {
+        // created a stack for storing the open parenthesis 
+        stack<int> st;
+        st.push(-1);            // initially push -1 to the stack because if there is ) then we can not pop our stack if it is empty
         
-        while(l<n){
+        int ans = 0;
+        
+        // iterate over the given string
+        for(int i=0;i<s.length();i++)
+        {
+            // if it is open parenthesis then push that index to the stack
+            if(s[i] == '(')
+                st.push(i);
             
-            
-            if(s[l]=='('){
-                o++;
-                l++;
-            }
-            else{
+            // if it is close then pop the stack 
+            // and check it is empty or not if it is empty then push current value to it because if there is another ) then we can not pop our stack if it is empty
+            // if it is not empty then find the length between the (current index i and stack top index) and store the max value in ans
+            else
+            {
+                st.pop();
                 
-                if(o>0){
-                    o--;
-                    l++;
-                }
-                else{
-                    l++;
-                    st = l;
-                }
-            }
-            
-            if(o==0){
-                ans = max(ans, l-st);
+                if(st.empty())
+                    st.push(i);
+                else
+                    ans = max(ans,i-st.top());
             }
         }
-        int c = 0;
-        st = n-1, l=n-1;
-        while(st>0){
-            
-            
-            if(s[st]==')'){
-                c++;
-                st--;
-            }
-            else{
-                
-                if(c>0){
-                    c--;
-                    st--;
-                }
-                else{
-                    st--;
-                    l = st;
-                }
-            }
-            
-            if(c==0){
-                ans = max(ans, l-st);
-            }
-        }
+        
+        // returning the answer
         return ans;
     }
 };
