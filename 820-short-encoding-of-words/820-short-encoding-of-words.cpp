2b7d26652c2ec3d827fdca04d45a1e2224824323
Hey, I am Aditya !!! <111901005@smail.iitpkd.ac.in>
2022-06-21 04:13:46 +0530

    Time: 163 ms (30.88%), Space: 68.2 MB (39.71%) - LeetHub

--- 820-short-encoding-of-words/820-short-encoding-of-words.cpp
+++ 820-short-encoding-of-words/820-short-encoding-of-words.cpp
@@ -1,22 +1,51 @@
-class TrieNode {
-public:
-    unordered_map<char, TrieNode *> next;
+
+class trieNode{
+    public:
+    unordered_map<char,trieNode*>next;
 };
+
 class Solution {
 public:
     int minimumLengthEncoding(vector<string>& words) {
-        TrieNode *root = new TrieNode;
-        vector<pair<TrieNode *, int>> leaves;
-        for (auto & w : unordered_set<string> (words.begin(), words.end())) {
-            TrieNode *cur = root;
-            for (int i = w.length() - 1; i >= 0; --i) {
-                if (cur->next.count(w[i]) == 0) cur->next[w[i]] = new TrieNode;
-                cur = cur->next[w[i]];
+        
+        trieNode* root = new trieNode();
+        
+        vector<pair<trieNode *, int>>leaves;
+            
+        for(auto &word:words){
+            
+            trieNode* cur = root;
+            bool fl = 0;
+            
+            for(int i=word.size();i>=0;i--){    
+                
+                fl = 0;
+                
+                // cout<<cur<<" "<<word[i]<<endl;
+                // cout<<" bnc:"<<cur->next.count(word[i])<<endl;
+                if(cur->next.count(word[i])==0) cur->next[word[i]] = new trieNode();
+                else fl = 1;
+                
+                // cout<<" nc:"<<cur->next.count(word[i])<<"fl:"<<fl<<endl;
+                cur = cur->next[word[i]];
             }
-            leaves.push_back(make_pair(cur, w.length() + 1));
+            
+            // cout<<"afl:"<<fl<<endl;
+            if (fl == 0){
+                // cout<<"pursed"<<endl;
+                leaves.push_back({cur,word.size()+1});
+            }
         }
-        int res = 0;
-        for (auto leaf : leaves) if ((leaf.first->next).size() == 0) res += leaf.second;
-        return res;
+        
+        int ans = 0;
+        
+        for(auto it:leaves){
+            
+            if(it.first->next.size()==0){
+                ans+=it.second;
+            }
+        }
+        
+        return ans;
     }
 };
