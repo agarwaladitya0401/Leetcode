423bf3e448636371232eb4d0be4bd92d835a57d2
Hey, I am Aditya !!! <111901005@smail.iitpkd.ac.in>
2022-06-28 03:20:30 +0530

    Time: 30 ms (89.99%), Space: 13.5 MB (65.01%) - LeetHub

---
+++ 1689-partitioning-into-minimum-number-of-deci-binary-numbers/1689-partitioning-into-minimum-number-of-deci-binary-numbers.cpp
@@ -0,0 +1,13 @@
+class Solution {
+public:
+    int minPartitions(string n) {
+        
+        int m = 0;
+        
+        for(auto it:n){
+            m = max(m,it-'0');
+        }
+        
+        return m;
+    }
+};
