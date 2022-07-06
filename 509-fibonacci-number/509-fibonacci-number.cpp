3590e44c50531a378aba19f97614f6faaab742f5
Hey, I am Aditya !!! <111901005@smail.iitpkd.ac.in>
2022-07-07 05:20:05 +0530

    Time: 0 ms (100.00%), Space: 6 MB (41.61%) - LeetHub

---
+++ 509-fibonacci-number/509-fibonacci-number.cpp
@@ -0,0 +1,20 @@
+class Solution {
+public:
+    int dp[31];
+    
+    int fib(int n) {
+        
+        if(n==0){
+            return 0;
+        }
+        if(n==1){
+            return 1;
+        }
+        
+        if(dp[n]>0){
+            return dp[n];
+        }
+        
+        return dp[n] = fib(n-1)+fib(n-2);
+    }
+};
