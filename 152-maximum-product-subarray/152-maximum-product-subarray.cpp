cb35d0f90ce12d71420df6aec17cce827c483bc9
Hey, I am Aditya !!! <111901005@smail.iitpkd.ac.in>
2022-08-13 10:57:05 +0530

    Time: 83 ms (5.02%), Space: 13.9 MB (26.08%) - LeetHub

--- 152-maximum-product-subarray/152-maximum-product-subarray.cpp
+++ 152-maximum-product-subarray/152-maximum-product-subarray.cpp
@@ -4,19 +4,81 @@ public:
      
         int n = nums.size();
         
-        int p1 = nums[0], p2 = nums[0], res = nums[0];
+//         int p1 = nums[0], p2 = nums[0], res = nums[0];
         
-        for(int i=1;i<n;i++){
+//         for(int i=1;i<n;i++){
             
-            int temp = max({nums[i],p1*nums[i],p2*nums[i]});
-            p2 = min({nums[i],p1*nums[i],p2*nums[i]});
+//             int temp = max({nums[i],p1*nums[i],p2*nums[i]});
+//             p2 = min({nums[i],p1*nums[i],p2*nums[i]});
             
-            p1 = temp;
+//             p1 = temp;
             
-            res = max({res,p1,p2});
+//             res = max({res,p1,p2});
             
+//         }
+        
+//         return res;
+        
+        int ng = 0;
+        int ps = 0;
+        for(auto it:nums){
+            if(it<0){
+                ng++;
+            }
+            else if(it>0){
+                ps++;
+            }
         }
         
-        return res;
+        long long int ans = nums[0];
+        long long int i = 0, j = 0, p = 1;
+        
+        while(j<n){
+            
+            
+            if(nums[j]==0){
+                if(p<0){
+                    while(p<0 and i<j){
+                        p/=nums[i];
+                        
+                        cout<<"1p:"<<p<<endl;
+                        i++;
+                        if(i<j)
+                        ans = max(ans,p);                        
+                        cout<<"ans2 "<<ans<<endl;
+                    }
+                    
+                }
+                
+                i = j+1;
+                ans = max(ans,0LL);
+                p = 1;
+                cout<<"ans21 "<<ans<<" p:"<<p<<" i:"<<i<<endl;
+                // j++;
+            }
+            else{
+                cout<<"p:"<<p<<endl;
+                p *= nums[j];
+                ans = max(ans,p);
+                cout<<"ans1 "<<ans<<endl;
+            }
+            
+            j++;
+        }
+        
+        while(p<0 and i<j){
+            p/=nums[i];
+            i++;
+            if(i<j)
+            ans = max(ans,p);
+            cout<<"ans3 "<<ans<<endl;
+        }
+        
+            ans = max(ans,(long long int)*max_element(nums.begin(),nums.end()));
+            cout<<"ans4 "<<ans<<endl;
+        
+        
+        return ans;
+ 
     }
 };
